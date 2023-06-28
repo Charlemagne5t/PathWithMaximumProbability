19503414677acf829681e4f0fb24ab74e84da9f5
alek <wait82985@gmail.com>
2023-06-28 19:02:04 +0600

    problem solved. Dejkstra algo

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,71 @@
+import java.util.*;
+
 public class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
-        //TOD0
+        // Dejkstra algo
+        //building a graph
+        Map<Integer, Map<Integer, Double>> graph = new HashMap<>();
+        for (int i = 0; i < edges.length; i++) {
+            int[] edge = edges[i];
+            int node1 = edge[0];
+            int node2 = edge[1];
+            double probability = succProb[i];
+
+            if (!graph.containsKey(node1)) {
+                graph.put(node1, new HashMap<>());
+            }
+            graph.get(node1).put(node2, probability);
+
+            if (!graph.containsKey(node2)) {
+                graph.put(node2, new HashMap<>());
+            }
+            graph.get(node2).put(node1, probability);
+
+        }
+
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingDouble(a -> -a.probabilityFromStart));
+        double[] probabilities = new double[n];
+        pq.offer(new Node(start, 1));
+
+        while (!pq.isEmpty()) {
+            Node currentNode = pq.poll();
+            if (currentNode.index == end) {
+                return currentNode.probabilityFromStart;
+            }
+
+            if (currentNode.probabilityFromStart > probabilities[currentNode.index]) {
+                probabilities[currentNode.index] = currentNode.probabilityFromStart;
+                Map<Integer, Double> neighbours = graph.get(currentNode.index);
+                if (neighbours != null) {
+                    for (Map.Entry<Integer, Double> neighbour : neighbours.entrySet()) {
+                        int neighbourIndex = neighbour.getKey();
+                        double probability = neighbour.getValue();
+                        pq.offer(new Node(neighbourIndex, currentNode.probabilityFromStart * probability));
+                    }
+                }
+            }
+
+        }
+
+
         return 0.0;
+    }
+}
+
+class Node {
+    int index;
+    double probabilityFromStart;
+
+    public Node(int index, double probabilityFromStart) {
+        this.index = index;
+        this.probabilityFromStart = probabilityFromStart;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "index=" + index +
+                ", probabilityFromStart=" + probabilityFromStart +
+                '}';
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -16,7 +16,7 @@ public class SolutionTest {
 
         double expected = 0.25000;
         double actual = new Solution().maxProbability(n, edges, succProb, start, end);
-        double delta = 0.000001;
+        double delta = 1e-5;
 
         Assert.assertEquals(expected, actual, delta);
     }
@@ -35,7 +35,7 @@ public class SolutionTest {
 
         double expected = 0.30000;
         double actual = new Solution().maxProbability(n, edges, succProb, start, end);
-        double delta = 0.000001;
+        double delta = 1e-5;
 
         Assert.assertEquals(expected, actual, delta);
     }
@@ -52,7 +52,31 @@ public class SolutionTest {
 
         double expected = 0.00000;
         double actual = new Solution().maxProbability(n, edges, succProb, start, end);
-        double delta = 0.000001;
+        double delta = 1e-5;
+
+        Assert.assertEquals(expected, actual, delta);
+    }
+
+    @Test
+    public void maxProbabilityTest4() {
+        int n = 5;
+
+        int[][] edges = {
+                {1, 4},
+                {2,4},
+                {0,4},
+                {0,3},
+                {0,2},
+                {2,3}
+        };
+
+        double[] succProb = {0.37,0.17,0.93,0.23,0.39,0.04};
+        int start = 3;
+        int end = 4;
+
+        double expected = 0.21390;
+        double actual = new Solution().maxProbability(n, edges, succProb, start, end);
+        double delta = 1e-5;
 
         Assert.assertEquals(expected, actual, delta);
     }
